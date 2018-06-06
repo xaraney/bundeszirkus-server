09e9805f44e032566e758f719619548765d1e499
Florian Baierl <fbaierl1@gmail.com>
2018-06-06 20:12:08 +0200

    testing bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+
+
 app.use(express.static('public'))
+
+app.get('/politicians', function(req, res){
+    res.send('angela merkel')
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
 
+  
