63aa371b68207651035d716e83288e0298f68d20
Florian Baierl <fbaierl1@gmail.com>
2018-06-09 19:25:49 +0200

    Fixed another problem regarding parsing of comments

--- dataLoader.js
+++ dataLoader.js
@@ -19,6 +19,16 @@ function findElements(element, xml) {
 }
 
 
+/**
+ * Returns a list of structured comment objects. 
+ * (One <kommentar>-tag may include multiple comments)
+ * e.g. 
+ * (Beifall bei der AfD – Martin Schulz [SPD]: Da kennt ihr euch ja aus!)
+ * ->
+ * [{fullName:"Martin Schulz", party:"SPD", text:"Da kennt ihr euch ja aus!"}]
+ * @param {*} comment the comment to categorize as a string 
+ * @returns []
+ */
 function structureComment2(comment){
 
 
@@ -27,11 +37,23 @@ function structureComment2(comment){
     let parts = comment.split("–")
 
     parts.forEach(function(part){
-        let r = /(?:\(|^)?(.*?) \[(.*?)\](?::|, an .+? gewandt:) (.*?)(?:\)|$)/g
+        /*
+        * Regex will result in capturing groups, e.g. for "(Beifall bei der AfD – Martin Schulz [SPD]: Da kennt ihr euch ja aus!)"
+        * 1. "Martin Schulz"
+        * 2. "SPD"
+        * 3. "Da kennt ihr euch ja aus!"
+        * 
+        * Some comments also include "an ... gewandt", e.g.
+        * "Dr. Volker Ullrich [CDU/CSU], an DIE LINKE gewandt: Wo ist denn Frau Wagenknecht heute?)"
+        * 
+        * Some comments also include the city a representative is from, e.g. 
+        * "Carsten Schneider [Erfurt] [SPD]: Wir sind es schon!"
+        * 
+        * Good resource for testing regex: regex101.com
+        */
+        let r = /(?:\(|^)?(.*?) (?:\[.+\] )?\[(.*?)\](?::|, an .+? gewandt:) (.*?)(?:\)|$)/g
         let match = r.exec(part);
 
-        // TODO Armin Schuster	Weil am Rhein] [CDU/CSU	Das macht keinen Spaß!
-        // Armin Schuster	Weil am Rhein] [CDU/CSU	Das würde ich aber auch empfehlen!
         if(match){
 
             let fullname = match[1].trim()
@@ -49,94 +71,6 @@ function structureComment2(comment){
     })
 
     return result;
-}
-
-
-
-
-
-/**
- * Returns a structured comment object
- * e.g. 
- * (Beifall bei der AfD – Martin Schulz [SPD]: Da kennt ihr euch ja aus!)
- * ->
- * {fullName:"Martin Schulz", party:"SPD", text:"Da kennt ihr euch ja aus!"}
- * @param {*} comment the comment to categorize as a string 
- */
-function structureComment(comment){ // TODO which party is talking? // TODO date
-    /*
-     * Regex will result in capturing groups, e.g. for "(Beifall bei der AfD – Martin Schulz [SPD]: Da kennt ihr euch ja aus!)"
-     * 2. "Martin Schulz"
-     * 3. "SPD"
-     * 4. "Da kennt ihr euch ja aus!"
-     * 
-     * Some comments also include "an ... gewandt", e.g.
-     * "Dr. Volker Ullrich [CDU/CSU], an DIE LINKE gewandt: Wo ist denn Frau Wagenknecht heute?)"
-     * 
-     * Good resource for testing regex: regex101.com
-     */
-    var myRegexp = /(?:\(|^)?(.*?) \[(.*?)\](?::|, an .+? gewandt:) (.*?)(?:\)|$)/g
-    var match = myRegexp.exec(comment);
-
-    if(match){
-
-        let fullname = match[1]
-        let party = match[2]
-        let text = match[3]
-
-        let result = []
-
-        if(party.includes("[")){
-            console.log(comment)
-        }
-
-        // Next, we have to deal with various special cases ... 
-
-        // ... in case fullname is e.g. "Gegenruf des Abg. Karsten Hilse"
-        fullname = fullname.replace(/Gegenrufe? de(s|r) Abg[.]?/, "").trim()
-                  
-        // ... in case fullname is e.g. "Abgeordnete der AfD erheben sich – Lachen bei der SPD und der LINKEN – Matthias W. Birkwald"
-        fullname = fullname.split("–").pop().trim() // for double names e.g. "Michael Grosse-Brömer" a different kind of dash is used
-
-        // ... in case fullname has a predeceding "Abg" or "Abg."
-        fullname = fullname.replace(/Abg.?/,"").trim()
-      
-        // ... in case text is e.g. "Nein, das sind kommunale Investitionen! – Gegenrufe des Abg. Eckhardt Rehberg [CDU/CSU]"
-        //                          "Das war gut! Treffer! – Christine Aschenberg-Dugnus [FDP]: Wir waren verantwortungsvoll!"
-        let textArr = text.split("–")
-        text = textArr.shift().trim()
-
-        result.push({fullname:fullname, party:party, text:text})
-        
-        textArr.forEach(function(potentialComment){
-            let additionalComments = structureComment(potentialComment.trim())
-            if(additionalComments){
-                additionalComments.forEach(function (c){
-                    if(c){
-                        result.push(c)
-                    }
-                })
-            }  
-        })
-        
-        // (Lachen bei der AfD – Dr. Alexander Gauland [AfD], an die CDU/CSU gewandt: Da seht ihr mal: Ihr kommt mit der Anpassung nicht weit! Da habt ihr den Salat! 
-        // – Gegenruf des Abg. Michael Brand [Fulda] [CDU/CSU]: Das ist nicht unsere Anpassung, Herr Gauland! Sie sind denen näher als uns!)
-
-        // {"fullname":"Dr. Alexander Gauland","party":"AfD","text":"„Faschistisch“ fehlt! – Gegenruf der Abg. Dagmar Ziegler [SPD]: Wenn Sie meinen, Herr Gauland!"}
-
-        // TODO "Das ist nicht unsere Anpassung, Herr Gauland! Sie sind denen näher als uns!" does not work at all
-        /*
-        // Beifall bei der CDU/CSU sowie des Carsten Schneider	Erfurt] [SPD] – Johannes Vogel [Olpe] [FDP	Bedenke das Ende, Peter! Bedenke das Ende!
-
-        // Beifall bei der CDU/CSU und der FDP sowie des Dr. Tobias Lindner	BÜNDNIS 90/DIE GRÜNEN] – Andreas Mattfeldt [CDU/CSU	Da hast du recht!
-        Beifall bei der CDU/CSU sowie bei ordneten der SPD und der AfD und der Abg. Katrin Göring-Eckardt	BÜNDNIS 90/DIE GRÜNEN] – Armin-Paulus Hampel [AfD	Dann hat sich die Aktuelle Stunde ja schon gelohnt, Herr Abgeordneter!
-*/
-
-        
-        return result;
-    }
-
-    return null
 }
 
 exports.comments = function(callback){
